cf55d79e31f93c445a1215a72e096dffb37912e0
Ryan Duff <ryanduff613@gmail.com>
2020-03-31 11:37:16 -0700

    succesfully evaluate whether user has entered a number using isNan. code is working, requirements complete.

--- js/scripts.js
+++ js/scripts.js
@@ -6,19 +6,7 @@ $(document).ready(function(){
     var number2 = parseInt($('#number2').val());
     console.log(number1, number2);
  
-    /*for (var index = 0; index <= number1; index += number2) {
-      if (number1=NaN || number2=NaN) {
-        alert('oops, you need to enter a number for this to work, please refresh page and try again');
-      } else if (number1 <= 0 || number2 <= 0) {
-        alert('sorry, this page only works with positive numbers');
-      } else if (number2 > number1) {
-        alert('you cannot count to ' + number1 + ' in increments of '+ number2 + ' because ' + number2 + ' is larger than ' + number1);
-      } else {
-        $('#output').append(index +',');
-      }
-    }*/
-
-    if (number1 === 'NaN' || number2 === 'NaN') {
+    if (isNaN(number1) || isNaN(number2)) {
       alert('oops, you need to enter a number for this to work, please refresh page and try again');
     } else if (number1 <= 0 || number2<=0) {
       alert('sorry, this page only works with positive numbers');
@@ -29,9 +17,6 @@ $(document).ready(function(){
         $('#output').append(index +',');
       }
     }
-
-
-    
 
   });
 });
