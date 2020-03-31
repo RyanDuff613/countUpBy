70000960d4b341323b001dc097a8deb6e135cf00
Ryan Duff <ryanduff613@gmail.com>
2020-03-31 10:29:08 -0700

    implement for loop using numbers provided by the user, code not working as expected

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,15 @@
 $(document).ready(function(){
 
   $('#theForm').submit(function(event){
+    event.preventDefault();
     var number1 = $('#number1').val();
     var number2 = $('#number2').val();
     console.log(number1, number2);
 
-    event.preventDefault();
+    for (var index = 0; index >= number1; index += number2) {
+      $('#output').text(index);
+      console.log(index);
+    }
+
   });
-  
 });
