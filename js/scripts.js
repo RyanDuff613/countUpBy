7736b9cb74b31352052c19943ee881e2e2bb76b7
Ryan Duff <ryanduff613@gmail.com>
2020-03-31 10:13:28 -0700

    add js to take in user provided numbers

--- js/scripts.js
+++ js/scripts.js
@@ -1,2 +1,11 @@
+$(document).ready(function(){
 
+  $('#theForm').submit(function(event){
+    var number1 = $('#number1').val();
+    var number2 = $('#number2').val();
+    console.log(number1, number2);
+
+    event.preventDefault();
+  });
   
+});
