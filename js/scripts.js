e484c004287839b1d142ae86eb0e4164b2995d3a
Ryan Duff <ryanduff613@gmail.com>
2020-03-31 10:52:35 -0700

    add parseInt to user provided number, succesfully use for loop

--- js/scripts.js
+++ js/scripts.js
@@ -2,13 +2,12 @@ $(document).ready(function(){
 
   $('#theForm').submit(function(event){
     event.preventDefault();
-    var number1 = $('#number1').val();
-    var number2 = $('#number2').val();
+    var number1 = parseInt($('#number1').val());
+    var number2 = parseInt($('#number2').val());
     console.log(number1, number2);
-
-    for (var index = 0; index >= number1; index += number2) {
-      $('#output').text(index);
-      console.log(index);
+ 
+    for (var index = 0; index <= number1; index += number2) {
+      console.log(number1, number2, index);
     }
 
   });
